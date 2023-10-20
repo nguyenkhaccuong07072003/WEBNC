bf63a04d372047bb985909e14525e7a5a2e43a9a
nguyenkhaccuong07072003 <124782853+nguyenkhaccuong07072003@users.noreply.github.com>
2023-10-20 15:21:23 +0700

    init

---
+++ BTLWeb/Controllers/NHACUNGCAPController.cs
@@ -0,0 +1,52 @@
+﻿using BTLWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTLWeb.Controllers
+{
+    public class NHACUNGCAPController : Controller
+    {
+        private Models.QLQCFEntities db = new Models.QLQCFEntities();
+        // GET: NHACUNGCAP
+        public ActionResult Info()
+        {
+            QLQCFEntities db = new QLQCFEntities();
+            List<NHACUNGCAP> ncc = db.NHACUNGCAP.ToList();
+            return View(ncc);
+        }
+        [HttpGet]
+        public ActionResult AddNCC()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddNCC (NHACUNGCAP ncc)
+        {
+            QLQCFEntities db = new QLQCFEntities();
+            db.NHACUNGCAP.Add(ncc);
+            db.SaveChanges();
+            return RedirectToAction("Info");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateNCC()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UpdateNCC(NHACUNGCAP obj)
+        {
+            var edncc = db.NHACUNGCAP.Find(obj.MANCC);
+            edncc.MANCC = obj.MANCC;
+            edncc.TENNCC = obj.TENNCC;
+            edncc.DIACHI = obj.DIACHI;
+            edncc.SDT = obj.SDT;
+            db.SaveChanges();
+
+            return RedirectToAction("Info");
+        }
+    }
+}
