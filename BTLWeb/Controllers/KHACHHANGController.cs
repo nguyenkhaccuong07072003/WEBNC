61c7a8c54563f6e1d43117aabeb48f72e68bbd41
Junnngggg <luongthuydung1803@gmail.com>
2023-10-21 23:02:49 +0700

    thêm controller KH

---
+++ BTLWeb/Controllers/KHACHHANGController.cs
@@ -0,0 +1,35 @@
+﻿using BTLWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTLWeb.Controllers
+{
+    public class KHACHHANGController : Controller
+    {
+        private Models.QLQCFEntities db = new Models.QLQCFEntities();
+        // GET: KHACHHANG
+        public ActionResult Info()
+        {
+            QLQCFEntities db = new QLQCFEntities();
+            List<KHACHHANG> kh = db.KHACHHANG.ToList();
+            return View(kh);
+        }
+
+        [HttpGet]
+        public ActionResult AddKH()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddKH(KHACHHANG kh)
+        {
+            QLQCFEntities db = new QLQCFEntities();
+            db.KHACHHANG.Add(kh);
+            db.SaveChanges();
+            return RedirectToAction("Info");
+        }
+    }
+}
